2d1fbc6f18f43ad98d4179da5eae9a1b18245e0a
Andrew Green <green.andrew.b@gmail.com>
2021-07-10 15:57:13 -0500

    package.json scripts

--- eventtranspotracker/settings.py
+++ eventtranspotracker/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'event-transpo-manager', "build", "static")
+    os.path.join(BASE_DIR, "build", "static")
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
