e54f9e25c89e543aabf896c9824c2421f0a57d48
Andrew Green <green.andrew.b@gmail.com>
2021-07-09 22:57:30 -0500

    openssl

--- eventtranspotracker/settings.py
+++ eventtranspotracker/settings.py
@@ -37,7 +37,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # CSRF_COOKIE_SECURE = True
 
