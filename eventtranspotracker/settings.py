58144d11176af8b07cbeb2963735526e24ec80f8
Andrew Green <green.andrew.b@gmail.com>
2021-07-12 15:09:11 -0500

    test

--- eventtranspotracker/settings.py
+++ eventtranspotracker/settings.py
@@ -39,13 +39,13 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-CSRF_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
-SESSION_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
 
-SECURE_SSL_REDIRECT = True
+# SECURE_SSL_REDIRECT = True
 
 # Application definition
 
